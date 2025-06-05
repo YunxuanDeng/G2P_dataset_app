0a6f704bd56c837425299156382ec350f2cbdddf
Tim Deng <Timdeng@ucdavis.edu>
2025-06-05 14:13:08 -0700

    6/5 update

---
+++ GPDatasets-main/Analyses/RKHS.R
@@ -0,0 +1,50 @@
+install.packages(pkg='BGLR',repos='https://cran.r-project.org/')
+install.packages('BGData')
+
+library(tidyverse)
+library(BGLR)
+library(data.table)
+library(BGData)
+
+# load data
+load(list.files(path = "../input", recursive = T, full.names = T, pattern = ".rdata")[1])
+
+# Impute genotypes
+geno <- preprocess(geno, center = T, impute = T)
+
+# Take the first trait on the phenotype data frame
+y <- pheno[, 2]
+
+# Remove NAs
+geno <- geno[!is.na(y),]
+yNA <- y <- y[!is.na(y)]
+
+# Partition 80% TRN-TST
+trn <- sample(seq_along(y), as.integer(length(y) * .8))
+tst <- setdiff(seq_along(y), trn)
+yNA[-trn] <- NA
+
+# Set model choice and hyperparameters
+# Reproducing Kernel Hilbert Space model (RKHS)
+model <- 'RKHS'
+nIter <- 150
+burnIn <- 15
+
+if (model == 'RKHS') {
+  G <- getG(geno)
+  LP <- list(list(K = G, model = 'RKHS'))
+}
+
+# Saving on a temporary directory
+PWD <- getwd()
+setwd(tempdir())
+
+# Running the model
+mod0 <- BGLR(y = yNA, ETA = LP, nIter = nIter, burnIn = burnIn, verbose = F)
+
+# Coming back to the original directory
+setwd(PWD)
+
+# Prediction Ability for this sample
+PA <- cor(y[-trn], mod0$yHat[-trn])
+plot(cor(y, mod0$yHat), main = PA)
